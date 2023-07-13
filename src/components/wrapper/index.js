e675bab3162b0cf7573a4004864e8ef9db9167e0
shuntian <978987373@qq.com>
2023-07-14 07:42:13 +0800

    complate work base layout

---
+++ src/components/wrapper/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const style = {
+  padding: '24px',
+  minHeight: '85vh',
+  maxWidth: '1200px',
+  margin: '0 auto',
+  width: '100%'
+};
+
+export default function Wrapper({children}) {
+  return (
+    <div className='content-wrapper' style={style}>
+      {children}
+    </div>
+  )
+}
--- src/pages/works/index.js
+++ src/pages/works/index.js
@@ -1,7 +1,48 @@
-import React from 'react'
+import { Tabs } from 'antd';
+import Search from 'antd/es/transfer/search';
+import React, { useCallback, useState } from 'react'
+import TemplateList from '../../components/template-list';
+import Wrapper from '../../components/wrapper';
+
+import './style.css';
+
+const items = [
+  {
+    key: 'my-work',
+    label: '我的作品',
+    children: (
+      <>
+        <div>我的作品</div>
+        <TemplateList />
+      </>
+    )
+  },
+  {
+    key: 'my-template',
+    label: '我的模版',
+    children: (
+      <>
+        <div>我的模版</div>
+        <TemplateList />
+      </>
+    )
+  }
+]
 
 export default function Works() {
+
+  const [active, setActive] = useState('my-work');
+  const onSearch = () => {};
+  const onChange = useCallback((key) => {
+    setActive(key);
+  }, [])
   return (
-    <div>Works</div>
+    <Wrapper>
+      <div className='work-title'>
+        <h2>我的作品和模版</h2>
+        <Search placeholder="input search text" onSearch={onSearch} style={{ width: 200 }} />
+      </div>
+      <Tabs defaultActiveKey={active} items={items} onChange={onChange}/>
+    </Wrapper>
   )
 }
